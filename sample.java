ee31aef8edbbd5031ba29a45ff7d19d272f9c9dd
devopsmay24batch <devopsmay24batch@gmail.com>
2024-05-14 10:38:07 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,9 @@
 #my second file to my repo
 #java code goes here
-public class HelloWorld {
- public static void main(String[] args) {
- System.out.println("Hello, World");
- }
- }
+class HelloWorld
+{
+    public static void main(String []args)
+    {
+        System.out.println("Java sample code that runs from Jenkins.");
+    }
+};
