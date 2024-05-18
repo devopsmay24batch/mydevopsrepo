6acba1c8ccb8066062e3d8eb77d2ad37eb3349c5
devopsmay24batch <devopsmay24batch@gmail.com>
2024-05-18 09:57:42 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -6,6 +6,6 @@ class HelloWorld
         System.out.println("Java sample code that runs from Jenkins.");
         System.out.println("Added second line to check Poll SCM option - one last time!!.");
     }
-};
+
 //execute java file today with the build step.
 //this is the demo before adding build step.
